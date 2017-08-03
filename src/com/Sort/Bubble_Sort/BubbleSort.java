5342c5067ed6e48ef9a1a83b427672508e477ac3
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-08-03 17:18:15 -0400

    Completed and Tested the Output

--- src/com/Sort/Bubble_Sort/BubbleSort.java
+++ src/com/Sort/Bubble_Sort/BubbleSort.java
@@ -1,10 +1,38 @@
 package com.Sort.Bubble_Sort;
 
-public class BubbleSort {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+ 
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
+    public static void main(String args[])
+    {
+    	// Program execution Starts from here on!!!
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90}; // This can also be given dynamically Using Scanner 
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
 }
