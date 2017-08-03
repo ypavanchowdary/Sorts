b0e04c481508a95c789017ea62eea426275d9ae2
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-08-03 19:52:36 -0400

    How to inster into a already Sorted Linked list Updated and Final

--- src/com/Sort/Insertion_Sort/Insertion_Sorted_Linked_List.java
+++ src/com/Sort/Insertion_Sort/Insertion_Sorted_Linked_List.java
@@ -1,6 +1,33 @@
 package com.Sort.Insertion_Sort;
 
 
+
+
+
+/*
+ * 
+ * Algorithm: 
+Let input linked list is sorted in increasing order.
+
+1) If Linked list is empty then make the node as
+   head and return it.
+2) If value of the node to be inserted is smaller 
+   than value of head node, then insert the node 
+   at start and make it head.
+3) In a loop, find the appropriate node after 
+   which the input node (let 9) is to be inserted. 
+   To find the appropriate node start from head, 
+   keep moving until you reach a node GN (10 in
+   the below diagram) who's value is greater than 
+   the input node. The node just before GN is the
+   appropriate  node (7).
+4) Insert the node (9) after the appropriate node
+   (7) found in step 3.
+ * 
+ * 
+ * 
+ */
+
 //Java Program to insert in a sorted list
 
 public class Insertion_Sorted_Linked_List {
