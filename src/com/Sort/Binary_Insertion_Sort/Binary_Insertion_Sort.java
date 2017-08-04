04c8f2d49bd9468aa606e98d29238e7ed8d89e91
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-08-03 20:03:09 -0400

    How to implement Binary Insertion Sort Stage 1 done but not working

--- src/com/Sort/Binary_Insertion_Sort/Binary_Insertion_Sort.java
+++ src/com/Sort/Binary_Insertion_Sort/Binary_Insertion_Sort.java
@@ -15,10 +15,36 @@ The algorithm as a whole still has a running worst case running time of O(n2) be
 
 public class Binary_Insertion_Sort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void sort(int a[],int n){
+        for (int i=0;i<n;++i){
+            int temp=a[i];
+            int left=0;
+            int right=i;
+            while (left<right){
+                int middle=(left+right)/2;
+                if (temp>=a[middle])
+                    left=right+1;
+                else
+                    right=middle;
+            }
+            for (int j=i;j>left;--j){
+                swap(a,j-1,j);
+            }
+        }
+    }
 
-	}
+    public static void main(String[] args){
+        int a[]=new int[]{12,10,34,23,9,7,8,5,6};
+        sort(a,a.length);
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+    public static void swap(int a[],int i,int j){
+        int k=a[i];
+        a[i]=a[j];
+        a[j]=k;
+    }
 
 }
 
